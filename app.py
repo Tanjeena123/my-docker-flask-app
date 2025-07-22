510d70fd19fd5b3d23275993d32ecf16424f4ac7
Tanjeena123 <tanjeena@goal-connect.com>
2025-07-22 17:15:56 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("Hello Tanjeena San")
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify(message="Hello, Tanjeena-san! The APi is running successfully."), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
